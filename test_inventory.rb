f17f725991e5eb67445f00fb83c2df19d921bc60
César Díaz <cesare.diaz@gmail.com>
2022-12-04 16:48:47 -0300

    - fixed tests

--- test_inventory.rb
+++ test_inventory.rb
@@ -7,7 +7,8 @@ require './inventory'
 class TestInventory < Minitest::Test
   def setup
     @data = '{"store":"ALDO Crossgates Mall","model":"CAELAN","inventory":29}'
-    @inventory = Inventory.new(@data)
+    @db = Redis.new(reconnect_attempts: [0, 0.25, 1])
+    @inventory = Inventory.new(@db, @data)
   end
 
   def test_store
@@ -24,21 +25,21 @@ class TestInventory < Minitest::Test
 
   def test_minimum_stock_qty
     @data = '{"store":"ALDO Crossgates Mall","model":"CAELAN","inventory":9}'
-    @inventory = Inventory.new(@data)
+    @inventory = Inventory.new(@db, @data)
 
     assert_equal @inventory.alert, :minimum
   end
 
   def test_medium_stock_qty
     @data = '{"store":"ALDO Crossgates Mall","model":"CAELAN","inventory":30}'
-    @inventory = Inventory.new(@data)
+    @inventory = Inventory.new(@db, @data)
 
     assert_equal @inventory.alert, :medium
   end
 
   def test_high_stock_qty
     @data = '{"store":"ALDO Crossgates Mall","model":"CAELAN","inventory":55}'
-    @inventory = Inventory.new(@data)
+    @inventory = Inventory.new(@db, @data)
 
     assert_equal @inventory.alert, :high
   end
